57ae8a43584ab9203b8bb6bc82d7fb4cb95a1c39
Chauke Nhlanhla <96581999+JayMusk@users.noreply.github.com>
2026-09-10 14:12:11 +0200

    Enhance Logo class with welcome message and formatting
    Updated the Welcome method to include a welcome message and improved console formatting.

--- Programe/Logo.cs
+++ Programe/Logo.cs
@@ -1,17 +1,31 @@
-﻿using System;
 namespace chartBot
 {
     class Logo
     {
+        //constructor to initialize the Logo class
         public Logo() { }
+        //logo to be displayed when the application starts
         public void Display()
+        {Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("   _____      _____    ____________  ___\r\n" +
+                              "  /     \\    /  _  \\  /   _____/|  |/ __|\r\n" +
+                              " /  \\ /  \\  /  /_\\  \\ \\_____  \\ |   _/  \r\n" +
+                              "/    Y    \\/    |    \\/        \\|    \\\r\n\\" +
+                              "____|__  /\\____|__  /_______  /|____ \\ \r\n" +
+                              "        \\/         \\/        \\/      \\/");
+            Console.ResetColor();
+        }
+        //welcome message to be displayed when the application starts
+        public void Welcome()
         {
-            Console.WriteLine("   _____      _____    _____________  __.\r\n" +
-                              "  /     \\    /  _  \\  /   _____/    |/ _|\r\n" +
-                              " /  \\ /  \\  /  /_\\  \\ \\_____  \\|      <  \r\n" +
-                              "/    Y    \\/    |    \\/        \\    |  \\ \r\n\\" +
-                              "____|__  /\\____|__  /_______  /____|__ \\\r\n" +
-                              "        \\/         \\/        \\/        \\/");
-        } 
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("=========================================================");
+            Console.ResetColor();
+            Console.WriteLine("Welcome to the Cyber Security Awareness Bot ");
+            Console.WriteLine("Developed by: Mask");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("=========================================================");
+            Console.ResetColor();
+        }   
     }
 }
